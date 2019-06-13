d6b11a9ccd70c0867c3ce62fe37c6ef8e2f4a8fd
Fuzzle <marc.falzon@gmail.com>
2019-06-13 23:29:06 +1000

    Added movement input handling

--- engine.py
+++ engine.py
@@ -6,24 +6,49 @@ https://www.reddit.com/r/roguelikedev/comments/bz6s0j/roguelikedev_does_the_comp
 """
 
 import tcod as libtcod
+import tcod.event
+from input_handlers import handle_keys
+
 
 def main():
-    screen_with = 80
+    screen_width = 80
     screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_with, screen_height, 'Fuzzle Roguelike',False)
+    libtcod.console_init_root(screen_width, screen_height, 'Fuzzle Roguelike', False)
+
+    con = libtcod.console_new(screen_width, screen_height)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
-        libtcod.console_set_default_foreground(0, libtcod.white)
-        libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+        libtcod.console_set_default_foreground(con, libtcod.white)
+        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
         libtcod.console_flush()
 
-        key = libtcod.console_check_for_keypress()
+        action = handle_keys(key)
 
-        if key.vk == libtcod.KEY_ESCAPE:
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx,dy = move
+            player_x += dx
+            player_y += dy
+
+        if exit:
             return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
 
 if __name__ == '__main__':
