498eb5a68673d34e2623b43d514192057acd39e1
Fuzzle <marc.falzon@gmail.com>
2019-06-14 21:56:21 +1000

    Added Tile rendering added functionality for blocked tiles.

--- engine.py
+++ engine.py
@@ -10,6 +10,7 @@ import tcod.event
 from input_handlers import handle_keys
 from entity import Entity
 from render_functions import clear_all, render_all
+from map_objects.game_map import GameMap
 
 
 def main():
@@ -17,6 +18,11 @@ def main():
     screen_height = 50
     map_width = 80
     map_height = 45
+
+    colours = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
@@ -29,12 +35,14 @@ def main():
 
     con = libtcod.console_new(screen_width, screen_height)
 
+    game_map = GameMap(map_width, map_height)
+
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
-        render_all(con, entities, screen_width, screen_height)
+        render_all(con, entities, game_map, screen_width, screen_height, colours)
         libtcod.console_flush()
 
         clear_all(con, entities)
@@ -47,7 +55,8 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx, dy)
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
 
         if exit:
             return True
--- render_functions.py
+++ render_functions.py
@@ -1,18 +1,24 @@
 import tcod as libtcod
 
-def render_all(con, entities, screen_width, screen_height):
+
+def render_all(con, entities, game_map, screen_width, screen_height, colours):
     """
-    draw all entities in the list
-    :param con:
-    :param entities:
-    :param screen_width:
-    :param screen_height:
-    :return:
+    draw all entities in the game map
     """
+    for y in range(game_map.height):
+        for x in range(game_map.width):
+            wall = game_map.tiles[x][y].block_sight
+
+            if wall:
+                libtcod.console_set_char_background(con, x, y, colours.get('dark_wall'), libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(con, x, y, colours.get('dark_ground'), libtcod.BKGND_SET)
+
     for entity in entities:
         draw_entity(con, entity)
 
     libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
 
 def clear_all(con, entities):
     for entity in entities:
@@ -27,8 +33,5 @@ def draw_entity(con, entity):
 def clear_entity(con, entity):
     """
     Erase the character that is represented by this object
-    :param con:
-    :param entity:
-    :return:
     """
     libtcod.console_put_char(con, entity.x, entity.y, ' ', libtcod.BKGND_NONE)
