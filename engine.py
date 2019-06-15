af10d598c6d0963998bc1bbe218b60208c14c2b8
Fuzzle <marc.falzon@gmail.com>
2019-06-15 17:51:31 +1000

    Partial part 6
    Basic monster AI and components implemented.

--- engine.py
+++ engine.py
@@ -13,6 +13,7 @@ from render_functions import clear_all, render_all
 from map_objects.game_map import GameMap
 from fov_functions import initialise_fov, recompute_fov
 from game_states import GameStates
+from components.fighter import Fighter
 
 
 def main():
@@ -38,7 +39,8 @@ def main():
         'light_ground': libtcod.Color(200, 180, 50)
     }
 
-    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True)
+    fighter_component = Fighter(hp=30, defense=2, power=5)
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, fighter=fighter_component)
     entities = [player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
@@ -102,8 +104,8 @@ def main():
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
-                if entity != player:
-                    print('The {} ponders the meaning of its existence!'.format(entity.name))
+                if entity.ai:
+                    entity.ai.take_turn(player, fov_map, game_map, entities)
 
             game_state = GameStates.PLAYERS_TURN
 
