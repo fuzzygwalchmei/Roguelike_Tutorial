56c173c52896a500eaadc35faf85ddb19b0a5276
Fuzzle <marc.falzon@gmail.com>
2019-06-15 22:49:15 +1000

    partial Part 7 health bar added and functioning message log added and functioning

--- engine.py
+++ engine.py
@@ -9,6 +9,7 @@ import tcod as libtcod
 import tcod.event
 
 from death_functions import kill_monster, kill_player
+from game_messages import MessageLog
 from input_handlers import handle_keys
 from entity import Entity, get_blocking_entities_at_location
 from render_functions import clear_all, render_all, RenderOrder
@@ -21,8 +22,16 @@ from components.fighter import Fighter
 def main():
     screen_width = 80
     screen_height = 50
+    bar_width = 20
+    panel_height = 7
+    panel_y = screen_height - panel_height
+
+    message_x = bar_width + 2
+    message_width = screen_width - bar_width - 2
+    message_height = panel_height - 1
+
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -50,6 +59,7 @@ def main():
     libtcod.console_init_root(screen_width, screen_height, 'Fuzzle Roguelike', False)
 
     con = libtcod.console_new(screen_width, screen_height)
+    panel = libtcod.console_new(screen_width, panel_height)
 
     game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities,
@@ -58,6 +68,8 @@ def main():
     fov_recompute = True
 
     fov_map = initialise_fov(game_map)
+
+    message_log = MessageLog(message_x, message_width, message_height)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -70,7 +82,8 @@ def main():
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
-        render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, colours)
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height,
+                   bar_width, panel_height, panel_y, colours)
         fov_recompute = False
 
         libtcod.console_flush()
@@ -113,7 +126,7 @@ def main():
             dead_entity = player_turn_result.get('dead')
 
             if message:
-                print(message)
+                message_log.add_message(message)
 
             if dead_entity:
                 if dead_entity == player:
@@ -121,7 +134,7 @@ def main():
                 else:
                     message = kill_monster(dead_entity)
 
-                print(message)
+                message_log.add_message(message)
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
@@ -133,7 +146,7 @@ def main():
                         dead_entity = enemy_turn_result.get('dead')
 
                         if message:
-                            print(message)
+                            message_log.add_message(message)
 
                         if dead_entity:
                             if dead_entity == player:
@@ -141,7 +154,7 @@ def main():
                             else:
                                 message = kill_monster(dead_entity)
 
-                            print(message)
+                            message_log.add_message(message)
 
                             if game_state == GameStates.PLAYER_DEAD:
                                 break
