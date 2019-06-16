00ca9b74ea6bbc92beac094cd4112e6c0d455ba6
Fuzzle <marc.falzon@gmail.com>
2019-06-16 20:59:01 +1000

    Part 8 completed
    Ability to use and drop items
    Item functionality (heal) available

--- engine.py
+++ engine.py
@@ -95,11 +95,14 @@ def main():
 
         clear_all(con, entities)
 
-        action = handle_keys(key)
+        action = handle_keys(key, game_state)
 
         move = action.get('move')
         pickup = action.get('pickup')
         show_inventory = action.get('show_inventory')
+        drop_inventory = action.get('drop_inventory')
+        inventory_index = action.get('inventory_index')
+
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
@@ -132,12 +135,25 @@ def main():
                 else:
                     message_log.add_message(Message('There is nothing to pick up here.', libtcod.yellow))
 
-        if show_inventory and game_state == GameStates.PLAYERS_TURN:
+        if show_inventory:
             previous_game_state = game_state
             game_state = GameStates.SHOW_INVENTORY
 
-        if exit:
+        if drop_inventory:
+            previous_game_state = game_state
+            game_state = GameStates.DROP_INVENTORY
+
+        if inventory_index is not None and previous_game_state != GameStates.PLAYER_DEAD and inventory_index < len(
+                player.inventory.items):
+            item = player.inventory.items[inventory_index]
+
             if game_state == GameStates.SHOW_INVENTORY:
+                player_turn_results.extend(player.inventory.use(item))
+            elif GameStates.DROP_INVENTORY:
+                player_turn_results.extend(player.inventory.drop_item(item))
+
+        if exit:
+            if game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
                 game_state = previous_game_state
             else:
                 return True
@@ -149,6 +165,8 @@ def main():
             message = player_turn_result.get('message')
             dead_entity = player_turn_result.get('dead')
             item_added = player_turn_result.get('item_added')
+            item_consumed = player_turn_result.get('consumed')
+            item_dropped = player_turn_result.get('dropped_item')
 
             if message:
                 message_log.add_message(message)
@@ -163,6 +181,13 @@ def main():
 
             if item_added:
                 entities.remove(item_added)
+                game_state = GameStates.ENEMY_TURN
+
+            if item_consumed:
+                game_state = GameStates.ENEMY_TURN
+
+            if item_dropped:
+                entities.append(item_dropped)
                 game_state = GameStates.ENEMY_TURN
 
         if game_state == GameStates.ENEMY_TURN:
